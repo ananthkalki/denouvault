8177a5909233eeca3de7295b64753cee92ae89d4
Ananth Kalki <ananthkalki@gmail.com>
2023-10-28 16:37:41 +0530

    header update and removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
-import Header from './Components/UI/Navigation/Header';
+import Header from './Components/Navigation/Header';
 
 function App() {
   return (
